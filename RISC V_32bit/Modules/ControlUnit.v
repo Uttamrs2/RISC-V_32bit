4e7d42d42284cc34d89802af03c820b98514060c
Uttamrs2 <uttamrajsingh176@gmail.com>
2025-11-22 23:33:21 +0530

    Update ControlUnit.v

--- RISC V_32bit/Modules/ControlUnit.v
+++ RISC V_32bit/Modules/ControlUnit.v
@@ -22,34 +22,32 @@
 
 module ControlUnit (
     input [6:0] opcode,      // 7-bit opcode from instruction
-    output reg branch, mem_read, mem_to_reg,
+    output reg branch,
     output reg [1:0] alu_op, // Determines ALU operation type
-    output reg mem_write, alu_src, reg_write
+    output reg reg_write
 );
     always @(*) begin
         // Default values
-        branch = 0; mem_read = 0; mem_to_reg = 0;
-        mem_write = 0; alu_src = 0; reg_write = 0;
+        branch = 0; 
+       reg_write = 0;
         alu_op = 2'b00;
 
         // Decode opcode (simplified example)
         case(opcode)
             7'b0110011: begin // R-type (ADD, SUB, MUL, etc.)
                 alu_op = 2'b10;
-                alu_src = 0;
+               
                 reg_write = 1;
             end
             7'b0000011: begin // Load (I-type)
                 alu_op = 2'b00;
-                alu_src = 1;
+               
                 reg_write = 1;
-                mem_read = 1;
-                mem_to_reg = 1;
+                
             end
             7'b0100011: begin // Store (S-type)
                 alu_op = 2'b00;
-                alu_src = 1;
-                mem_write = 1;
+               
             end
             7'b1100011: begin // Branch (B-type)
                 alu_op = 2'b01;
